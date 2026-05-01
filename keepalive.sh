f75833738d8db6228275307332c4c47c0f9408b8
Z User <z@container>
2026-05-01 09:06:02 +0000

    cbdeea69-3b4d-4217-9441-1447965a66d7

---
+++ keepalive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/z/my-project
+while true; do
+  echo "$(date) Starting Next.js..." >> /home/z/my-project/server.log
+  npx next dev -p 3000 >> /home/z/my-project/server.log 2>&1
+  echo "$(date) Next.js exited, restarting in 2s..." >> /home/z/my-project/server.log
+  sleep 2
+done
